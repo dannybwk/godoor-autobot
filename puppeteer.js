f6d8aec682ff3c8f16f6f7cdd12acd1a6159fc78
dannybwk <smallbe@gmail.com>
2025-05-31 17:37:49 +0800

    Update puppeteer.js

--- puppeteer.js
+++ puppeteer.js
@@ -1,25 +1,13 @@
-
-const puppeteer = require('puppeteer');
-
-async function autoPostActivity(data) {
-  const browser = await puppeteer.launch({ headless: 'new' });
-  const page = await browser.newPage();
-
-  await page.goto('https://mg.umita.tw/login');
-  await page.type('input[name="account"]', '果多');
-  await page.type('input[name="password"]', '000');
-  await Promise.all([
-    page.waitForNavigation(),
-    page.click('button[type="submit"]')
-  ]);
-
-  await page.goto('https://mg.umita.tw/event/form');
-
-  await page.type('input[name="title"]', data.title || '未命名活動');
-  await page.type('textarea[name="description"]', data.description || '無活動內容');
-  // ...更多欄位可依需求擴充
-
-  await browser.close();
-}
-
-module.exports = autoPostActivity;
+const browser = await puppeteer.launch({
+  headless: 'new',
+  args: [
+    '--no-sandbox',
+    '--disable-setuid-sandbox',
+    '--disable-dev-shm-usage',
+    '--disable-accelerated-2d-canvas',
+    '--no-first-run',
+    '--no-zygote',
+    '--single-process',
+    '--disable-gpu'
+  ]
+});
