1266fff51171f1263e2493a5948ed7fa61571f91
dannybwk <smallbe@gmail.com>
2025-05-31 17:36:48 +0800

    Update index.js

--- index.js
+++ index.js
@@ -16,7 +16,7 @@ app.post('/api/submit', async (req, res) => {
   }
 });
 
-const PORT = process.env.PORT;
+const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
 });
